43a50cc670a4f21d5db8e7f282a807feb2b74f73
huxwell.src <niko2000gb@gmail.com>
2022-11-02 21:55:17 -0300

    Avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from './Components/Nav'
-import Inicio from './Components/inicio'
+import Inicio from './Components/Inicio'
 import Skills from './Components/Skills'
 
 const App = () => {
