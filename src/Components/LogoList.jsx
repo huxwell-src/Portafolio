cc8dcb619ef0a2c22cc2bc5838e204a45e9cface
huxwell <65736835+huxwell-src@users.noreply.github.com>
2022-11-22 13:45:54 -0300

    mejora icons

--- src/Components/LogoList.jsx
+++ src/Components/LogoList.jsx
@@ -2,11 +2,11 @@ import { Tooltip, Button } from "@material-tailwind/react";
 
 export const LogoList = ({ logo }) => {
   return (
-    <div className="2xl:w-[75%] w-full m-auto">
+    <div className="2xl:w-[75%] w-[105%] md:w-full  m-auto">
         {logo.map(logo =>(
             <Tooltip content={logo.title} >
                 <a href={logo.link}  target="_blank" key={logo.id} >
-                    <img src={logo.image} className="inline-block m-5"  width={100} />
+                    <img src={logo.image} className="inline-block md:m-5 m-4"  width={100} />
                 </a>
             </Tooltip>
         ))}
