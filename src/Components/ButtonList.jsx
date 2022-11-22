bbf022b7f967481116c8795d13485de8cfff182e
huxwell <65736835+huxwell-src@users.noreply.github.com>
2022-11-22 13:50:17 -0300

    Mejora botones

--- src/Components/ButtonList.jsx
+++ src/Components/ButtonList.jsx
@@ -5,7 +5,7 @@ export const ButtonList = ({categories, filterCategory}) => {
             <button
                 key={category}
                 onClick ={()=> filterCategory(category)}
-                className= "bg-gray mx-4 px-6  mt-4 rounded-full h-10 text-light-blue font-semibold focus:bg-light-blue focus:text-gray focus:shadow-lg hover:shadow-md transition-all duration-200 ">
+                className= "bg-gray md:mx-4 mx-0.5 px-6  mt-4 rounded-full h-10 text-light-blue font-semibold focus:bg-light-blue focus:text-gray focus:shadow-lg hover:shadow-md transition-all duration-200 ">
                 {category}
             </button>
         ) )}
