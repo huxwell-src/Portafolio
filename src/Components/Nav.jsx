784926f568d50ed66671dca0217a63866ddc8084
huxwell.src <niko2000gb@gmail.com>
2022-11-29 01:19:53 -0300

    HM

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -95,10 +95,10 @@ export default function NavBar() {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              className="h-6 w-6"
+              className="h-9 w-9"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              strokeWidth={2}
+              strokeWidth={3}
             >
               <path
                 strokeLinecap="round"
@@ -109,10 +109,11 @@ export default function NavBar() {
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
+              className="h-9 w-9"
               fill="none"
               stroke="currentColor"
-              strokeWidth={2}
+              viewBox="0 0 24 24"
+              strokeWidth={3}
             >
               <path
                 strokeLinecap="round"
